aa2389cdf53e0c3f29c2d7c67162ad3c4bd4edc2
Vitalii Besporstov <vitaliibesporstov@gmail.com>
2019-02-28 23:22:28 -0800

    Add ability to upload local file

--- src/components/UrlForm.js
+++ src/components/UrlForm.js
@@ -2,11 +2,8 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import { useGlobalState } from "./GlobalState";
 import Events from "../requests/events";
+import GetLocalFile from "../requests/events";
 import processData from "../services/processData";
-
-const refreshPage = () => {
-  window.location.reload();
-};
 
 const UrlForm = props => {
   const [loading, updateLoading] = useGlobalState("loading");
@@ -22,6 +19,8 @@ const UrlForm = props => {
 
     const formData = new FormData(currentTarget);
     const newUrl = formData.get("url");
+    const newFile = formData.get("file");
+
     updateLoading(true);
     if (newUrl) {
       Events.getFile(newUrl)
@@ -38,6 +37,19 @@ const UrlForm = props => {
       currentTarget.reset();
       props.history.push("/calendar");
     }
+    if (newFile) {
+      let reader = new FileReader();
+      reader.readAsText(newFile, "UTF-8");
+      reader.onload = evt => {
+        let events = evt.target.result;
+        updateTotalEventsCount(events.length);
+        events = processData(events);
+        updateEvents(events);
+        updateLoading(false);
+        currentTarget.reset();
+        props.history.push("/calendar");
+      };
+    }
   };
 
   return (
@@ -45,6 +57,11 @@ const UrlForm = props => {
       <form className="NavForm" onSubmit={handleSubmit}>
         <p>Open Calendar from URL</p>
         <input id="url" name="url" type="link" />
+        <Button color="inherit" variant="contained" type="submit">
+          Open
+        </Button>
+        <p>Open Calendar from computer</p>
+        <input type="file" name="file" />
         <Button color="inherit" variant="contained" type="submit">
           Open
         </Button>
